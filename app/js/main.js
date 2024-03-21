3ae837f4482077deebe40081de691f12bae48687
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2024-03-22 00:27:05 +0200

    start menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,21 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+
+  function mobileMenu() {
+    const burger = document.querySelector('.burger'),
+          mobileMenu = document.querySelector('.mobile-menu');
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      mobileMenu.classList.toggle('active');
+      document.querySelector('body').classList.toggle('lock');
+    });
+  }
+
+  if (document.querySelector('.burger')) {
+    mobileMenu();
+  }
+
+
+
 });
