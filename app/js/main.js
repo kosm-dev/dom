d09957fb3b9ee7a2714c407c09d78442e1b94775
kosm-dev <72253360+kosm-dev@users.noreply.github.com>
2024-03-23 03:01:58 +0200

    added section hero

--- app/js/main.js
+++ app/js/main.js
@@ -2,20 +2,48 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-
-  function mobileMenu() {
-    const burger = document.querySelector('.burger'),
-          mobileMenu = document.querySelector('.mobile-menu');
-    burger.addEventListener('click', () => {
-      burger.classList.toggle('active');
-      mobileMenu.classList.toggle('active');
-      document.querySelector('body').classList.toggle('lock');
-    });
-  }
-
   if (document.querySelector('.burger')) {
-    mobileMenu();
+    const burger = document.querySelector('.burger'),
+          burgerClose = document.querySelector('.burger-close'),
+          mobileMenu = document.querySelector('.mobile-menu');
+
+    function mobileMenuToggle(trigger, menu) {
+      trigger.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        document.querySelector('body').classList.toggle('lock');
+      });
+    }
+
+    mobileMenuToggle(burger, mobileMenu);
+    mobileMenuToggle(burgerClose, mobileMenu);
   }
+
+
+  // slider
+
+
+  const heroSlider = new Swiper('.hero-slider', {
+    // Optional parameters
+    loop: true,
+    effect: "fade",
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000
+    },
+
+    navigation: {
+      nextEl: '.hero-slider__next',
+      prevEl: '.hero-slider__prev',
+    },
+  });
+
+  
+
+
+
 
 
 
